48154c819465cc934277250bb50551a9e3647eba
Talha Jamal <chrisLandson057@gmail.com>
2024-09-23 13:08:30 +0500

    *

--- portfolio-website/script.js
+++ portfolio-website/script.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links li');
 
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('nav-active');
-    hamburger.classList.toggle('active'); // For the hamburger transformation animation
+  navLinks.classList.toggle('nav-active');
+  hamburger.classList.toggle('active');
+});
+
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navLinks.classList.remove('nav-active');
+    hamburger.classList.remove('active');
+  });
 });
